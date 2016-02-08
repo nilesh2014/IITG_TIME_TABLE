a073531c92c59299358aad4a15270f0ac70e644d
nilesh2014 <nilesh.vaishnav021297@gmail.com>
2016-02-08 23:11:02 +0530

    some modification in a.php

--- a.php
+++ a.php
@@ -3,14 +3,11 @@
   	<head>
     <meta charset="utf-8">
     <title></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link href="./css/bootstrap.css" rel="stylesheet">
-    <link href="./css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="./css/docs.css" rel="stylesheet">
+  
 <style>
 
 </style>
+</head>
     <body style="background-color:yellow;">
          
 	<div class="row-fluid">
