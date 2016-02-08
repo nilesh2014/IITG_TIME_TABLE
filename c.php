2be0eb0ef33b5af62a54e908c7389f3165db5949
nilesh2014 <nilesh.vaishnav021297@gmail.com>
2016-02-08 23:19:02 +0530

    new commit

--- c.php
+++ c.php
@@ -48,12 +48,7 @@ catch(PDOException $e)
   	<head>
     <meta charset="utf-8">
     <title></title>
-   <!--  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link href="./css/bootstrap.cssm" rel="stylesheet">
-    <link href="./css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="./css/docs.css" rel="stylesheet"> -->
-<style>
+ <style>
 
 </style>
     <body style="background-color:yellow;">
